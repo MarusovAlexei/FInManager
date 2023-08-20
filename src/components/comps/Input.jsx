03e3f35b90c8730b6cfd00abdf049cee52eea35b
Alexei <marusovalexei@gmail.com>
2023-08-20 23:02:51 +0300

    add: add form

--- src/components/comps/Input.jsx
+++ src/components/comps/Input.jsx
@@ -1,7 +1,20 @@
 import React from "react";
+import css from "../../styles/form.css";
 
-const Input = () => {
-  return <React.Fragment></React.Fragment>;
+const { Input } = css;
+
+const InputComponent = (props) => {
+  const { placeholder, maxLength } = props.params;
+
+  return (
+    <React.Fragment>
+      <Input
+        type={"text"}
+        placeholder={placeholder}
+        maxLength={maxLength}
+      ></Input>
+    </React.Fragment>
+  );
 };
 
-export default Input;
+export default InputComponent;
--- src/components/pages/Main.jsx
+++ src/components/pages/Main.jsx
@@ -1,11 +1,29 @@
 import React from "react";
 import Head from "../views/global/Head";
 import Foot from "../views/global/Foot";
+import InputComponent from "../comps/Input";
+import css from "../../styles/form.css";
+
+const { FormContainer, Button } = css;
 
 const Main = () => {
   return (
     <React.Fragment>
       <Head></Head>
+      <FormContainer>
+        <InputComponent
+          params={{ placeholder: "Введите сумму транзакции", maxLength: "100" }}
+        />
+        <InputComponent
+          params={{ placeholder: "Введите тип транзакции", maxLength: "100" }}
+        />
+        <InputComponent
+          params={{ placeholder: "Введите комментарий", maxLength: "100" }}
+        />
+        <Button backgroundColor={"rgb(229, 229, 229);"}>
+          Сохранить транзакцию
+        </Button>
+      </FormContainer>
       <Foot></Foot>
     </React.Fragment>
   );
